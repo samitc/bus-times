b07c2846c2f5533be61c05a7107e6d4d95e3048d
Amit Cirt <siamitcs0@gmail.com>
2023-01-02 01:02:41 +0200

    Always show first 50 stations.

--- src/ui/Select/SelectComponent.jsx
+++ src/ui/Select/SelectComponent.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useMemo, useState } from "react";
 import Select from "react-select";
 const SIZE_OF_OPTION = 34;
 const LAST_OPTION_MARGIN = 14;
-export default function SelectComponent({ items, value, maxShownItems, isMulti, placeholder, emptyFilterValue, filterFunction, onOpen, onClose, onValueChange, onInputChange }) {
-    const inputChange = onInputChange && ((input, action) => {
-        if (action.action === "input-change") {
-            onInputChange(input);
-        }
-    });
-    maxShownItems = maxShownItems || 4;
-    const maxHeight = maxShownItems * SIZE_OF_OPTION + LAST_OPTION_MARGIN;
-    return <Select
-        options={items || []}
-        value={value}
-        maxMenuHeight={maxHeight}
-        placeholder={placeholder}
-        noOptionsMessage={() => emptyFilterValue}
-        onInputChange={inputChange}
-        onChange={onValueChange}
-        isMulti={isMulti}
-        filterOption={filterFunction}
-        onMenuOpen={onOpen}
-        onMenuClose={onClose}
-        hideSelectedOptions={true}
-        isRtl={true}
+export default function SelectComponent({
+  items,
+  value,
+  maxShownItems,
+  isMulti,
+  placeholder,
+  emptyFilterValue,
+  filterFunction,
+  onOpen,
+  onClose,
+  onValueChange,
+  onInputChange,
+  maxItems,
+}) {
+  const [input, setInput] = useState("");
+  const inputChange = (input, action) => {
+    if (action.action === "input-change") {
+      setInput(input);
+      onInputChange && onInputChange(input);
+    }
+  };
+  maxShownItems = maxShownItems || 4;
+  const maxHeight = maxShownItems * SIZE_OF_OPTION + LAST_OPTION_MARGIN;
+  const filteredItems = useMemo(() => {
+    return items && filterFunction
+      ? items.filter((item) => filterFunction(item, input))
+      : items;
+  }, [items, filterFunction, input]);
+  const shownItems = useMemo(() => {
+    return filteredItems && maxItems
+      ? filteredItems.slice(0, maxItems)
+      : filteredItems;
+  }, [filteredItems, maxItems]);
+  return (
+    <Select
+      options={shownItems || []}
+      value={value}
+      maxMenuHeight={maxHeight}
+      placeholder={placeholder}
+      noOptionsMessage={() => emptyFilterValue}
+      onInputChange={inputChange}
+      onChange={onValueChange}
+      isMulti={isMulti}
+      onMenuOpen={onOpen}
+      onMenuClose={onClose}
+      hideSelectedOptions={true}
+      isRtl={true}
     />
+  );
 }
--- src/ui/Stations/StationsComponent.jsx
+++ src/ui/Stations/StationsComponent.jsx
@@ -1,32 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import SelectComponent from '../Select/SelectComponent';
-import { filterStation } from '../../utils/Stations';
-import { isMobile } from '../../utils/env';
-export default function StationsComponent({ stations, onInputChange, onValueChange, ...props }) {
-    const [selectedStations, setSelectedStations] = useState(null);
-    useEffect(() => {
-        setSelectedStations(null);
-    }, [stations])
-    const inputChanged = input => {
-        if (input.length <= 2) {
-            setSelectedStations(null);
-        } else {
-            setSelectedStations(stations && stations.filter(station => filterStation(station, input)));
-        }
-        onInputChange && onInputChange(input);
-    };
-    const valueChange = stations => {
-        setSelectedStations(null);
-        onValueChange && onValueChange(stations);
-    }
-    return <SelectComponent
-        items={selectedStations}
-        onInputChange={inputChanged}
-        onValueChange={valueChange}
-        maxShownItems={isMobile() ? 4 : 7}
-        placeholder='בחר תחנה'
-        emptyFilterValue='הקלד כדי לחפש'
-        filterFunctions={filterStation}
-        {...props}
+import React, { useState, useEffect } from "react";
+import SelectComponent from "../Select/SelectComponent";
+import { filterStation } from "../../utils/Stations";
+import { isMobile } from "../../utils/env";
+const MAX_STATION_LENGTH = 50;
+export default function StationsComponent({ stations, ...props }) {
+  return (
+    <SelectComponent
+      items={stations}
+      maxShownItems={isMobile() ? 4 : 7}
+      placeholder="בחר תחנה"
+      emptyFilterValue="הקלד כדי לחפש"
+      filterFunction={filterStation}
+      maxItems={MAX_STATION_LENGTH}
+      {...props}
     />
+  );
 }
